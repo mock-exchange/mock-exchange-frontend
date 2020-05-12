eb4ac00ddbf3b343bfd4d72b6c2b88bf67f96679
Eric Herrera <eric.herrera@hurloe.com>
2020-05-12 01:48:11 -0400

    trading cleanup

--- src/routes/trade/[market].svelte
+++ src/routes/trade/[market].svelte
@@ -5,9 +5,14 @@
 <script>
     console.log("trade.svelte module init part");
 
+    import { format } from 'date-fns'
+
     import { goto, stores } from '@sapper/app';
 
     const { page } = stores();
+
+    const options2 = { style: 'currency', currency: 'USD' };
+    const numberFormat2 = new Intl.NumberFormat('en-US', options2);
 
     import { onMount } from "svelte";
     import { owner_name } from '../../store.js'
@@ -20,7 +25,6 @@
 
     import Textfield, {Input, Textarea} from '@smui/textfield';
     import HelperText from '@smui/textfield/helper-text/index';
-
 
   import Menu, {SelectionGroup, SelectionGroupIcon} from '@smui/menu';
   import {Anchor} from '@smui/menu-surface';
@@ -70,7 +74,7 @@
 
 
     let owner_id;
-
+    let side = 'buy'
 
     onMount(async () => {
         console.log("trade.svelte module onMount");
@@ -90,7 +94,7 @@
 
         console.log('owner_id:'+owner_id);
 
-        fetch(`/api/order?status=open&owner=${owner_id}`)
+        fetch(`/api/order?status__in=new,open&owner=${owner_id}`)
         .then(r => r.json())
         .then(data => {
             active_orders = data;
@@ -122,6 +126,60 @@
         //console.log('market:'+market);
     }
 
+    async function handleOrderSubmit(e) {
+        console.log("handleOrderSubmit")
+        
+        var fe = document.forms.order_form.elements;
+
+        var orderdata = {
+            price: fe['price'].value,
+            amount: fe['amount'].value,
+            market: 1,
+            owner: 1,
+            direction: side
+        }
+        var formdata = {
+            owner: 1,
+            action: 'ao',
+            payload: JSON.stringify(orderdata)
+        }
+        fetch(`/api/event`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formdata)
+        })
+        .then(r => r.json())
+        .then(data => {
+            console.log('posted! data:',data)
+        });
+
+    }
+
+    async function handleOrderCancel(order_id) {
+        console.log("handleOrderCancel() order_id:"+order_id);
+        var orderdata = {
+            order_id: order_id
+        }
+        var formdata = {
+            owner: 1,
+            action: 'co',
+            payload: JSON.stringify(orderdata)
+        }
+        fetch(`/api/event`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formdata)
+        })
+        .then(r => r.json())
+        .then(data => {
+            console.log('posted! data:',data)
+        });
+
+    }
 
 </script>
 <style>
@@ -150,6 +208,10 @@
 }
 .rightside {
     padding-top: 1em;
+}
+.rightside .btn-small {
+    line-height: 20px;
+    height: 20px;
 }
 
 </style>
@@ -215,36 +277,40 @@
 <h2>Active orders</h2>
 
 {#if active_orders}
-<DataTable table$aria-label="Order list" style="width: 100%;">
-  <Head>
-    <Row>
-      <Cell>Id</Cell>
-      <Cell>Market</Cell>
-      <Cell>Type</Cell>
-      <Cell>Direction</Cell>
-      <Cell>Price</Cell>
-      <Cell>Amt</Cell>
-      <Cell>Amt Left</Cell>
-      <Cell>Balance</Cell>
-      <Cell>Status</Cell>
-    </Row>
-  </Head>
-  <Body>
+<table class="striped">
+  <thead>
+    <tr>
+      <th>Order</th>
+      <th>Opened</th>
+      <th>Type</th>
+      <th>Market</th>
+      <th class="right-align">Price</th>
+      <th>Amount</th>
+      <th>Cost</th>
+      <th>Status</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
     {#each active_orders as o }
-    <Row>
-      <Cell>{o.id}</Cell>
-      <Cell>{o.market}</Cell>
-      <Cell>{o.type}</Cell>
-      <Cell>{o.direction}</Cell>
-      <Cell>{o.price}</Cell>
-      <Cell>{o.amount}</Cell>
-      <Cell>{o.amount_left}</Cell>
-      <Cell>{o.balance}</Cell>
-      <Cell>{o.status}</Cell>
-    </Row>
+    <tr>
+      <td>{ o.id.toString().padStart(8, '0') }</td>
+      <td>{ format(Date.parse(o.created), 'yyyy-mm-dd hh:mm:ss aaa') }</td>
+      <td>{o.direction}/{o.type}</td>
+      <td>{o.market.name}</td>
+      <td class="right-align">{ numberFormat2.format(o.price) }</td>
+      <td>
+        {o.amount_left}/{o.amount}
+      </td>
+      <td>0</td>
+      <td>{o.status}</td>
+      <td>
+        <button on:click={() => handleOrderCancel(o.id)} class="btn-small">Cancel</button>
+      </td>
+    </tr>
     {/each}
-  </Body>
-</DataTable>
+  </tbody>
+</table>
 {:else}
   <p class="loading">loading...</p>
 {/if}
@@ -254,10 +320,19 @@
 
   </div>
   <div class="col m3 rightside">
+    <form name="order_form" on:submit|preventDefault="{handleOrderSubmit}">
     <div class="row">
       <ul class="tabs tabs-fixed-width">
-        <li class="tab"><a href="#order_buy" class="green-text">Buy</a></li>
-        <li class="tab"><a href="#order_sell" class="active red-text">Sell</a></li>
+        <li class="tab">
+          <a href="#order_buy" on:click={() => side = 'buy'}
+            class="green-text"
+            class:active={side === 'buy'}
+          >Buy</a></li>
+        <li class="tab">
+          <a href="#order_sell" on:click={() => side = 'sell'}
+            class="red-text"
+            class:active={side === 'sell'}
+          >Sell</a></li>
       </ul>
     </div>
     <div class="row">
@@ -265,18 +340,31 @@
         <div id="order_sell" class="col s12">Sell order form</div>
     </div>
     <div class="row">
+        <div class="right-align col s12">
+            <button class="btn-small btn-flat grey white-text">Bid</button>
+            <button class="btn-small btn-flat grey white-text">Ask</button>
+            <button class="btn-small btn-flat grey white-text">Last</button>
+        </div>
+
         <div class="input-field col s12">
-          <input id="price" type="text" class="validate">
+          <input id="price" name="price" type="text" class="validate">
           <label for="price">Price</label>
         </div>
     </div>
     <div class="row">
         <div class="input-field col s12">
-          <input id="amount" type="text" class="validate">
+          <input id="amount" name="amount" type="text" class="validate">
           <label for="amount">Amount</label>
         </div>
     </div>
     <div class="row">
+        <div class="right-align col s12">
+            <button class="btn-small btn-flat grey white-text">25%</button>
+            <button class="btn-small btn-flat grey white-text">50%</button>
+            <button class="btn-small btn-flat grey white-text">75%</button>
+            <button class="btn-small btn-flat grey white-text">100%</button>
+        </div>
+
         <div class="input-field col s12">
           <input id="total" type="text" class="validate">
           <label for="total">Total</label>
@@ -285,8 +373,15 @@
 
 
     <div>
-        <button class="btn green col s12">Place buy order</button>
+        <button 
+          class="btn col s12"
+          class:red={side === 'sell'}
+          class:green={side === 'buy'}
+        >Place {side} order</button>
     </div>
+    </form>
+
+
   </div>
 </div>
 
