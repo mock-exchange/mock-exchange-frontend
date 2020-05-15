faf95a4fd2d0bea613b69bb6c8eeef860a8a3083
Eric Herrera <eric.herrera@hurloe.com>
2020-05-14 22:28:19 -0400

    brought in all depends, except fa and material fonts

--- src/client.js
+++ src/client.js
@@ -1,5 +1,9 @@
 import * as sapper from '@sapper/app';
 
+import "./style/materialize/js/bin/materialize.min.js";
+//import "lightweight-charts";
+import "../node_modules/lightweight-charts/dist/lightweight-charts.standalone.production.js";
+
 sapper.start({
   target: document.querySelector('#sapper')
 });
