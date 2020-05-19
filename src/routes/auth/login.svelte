ef357d8a5e296588271804e1dd14404eb8c3dafa
Eric Herrera <eric.herrera@hurloe.com>
2020-05-19 15:26:27 -0400

    cleaned up

--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -114,69 +114,48 @@
 .collection-item.avatar {
   min-height: 0;
 }
-.card-panel {
+.card {
   margin-top: 50px;
 }
 .card-panel h1 {
   margin-top: 0;
 }
-
-.ac-dropdown {
-  display: block;
-  width: 384.5px;
-  left: 11.25px;
-  top: 46px;
-
-  transform-origin: 0px 0px;
-  opacity: 1;
-  transform: scaleX(1) scaleY(1);
-
-  /*
-  height: 50px;
-  */
-}
-.highlight {
-  background-color: #ccc;
-}
 </style>
 
-<div class="card-panel">
-
-<h1>Login</h1>
 
 <form name="login_form" on:submit|preventDefault={login}>
+  <div class="card">
 
-<div class="row">
-  <div class="input-field col s12">
+    <div class="card-content">
+      <span class="card-title mex-primary-text-color">MEX Login</span>
+      <div class="row">
+        <div class="input-field col s12">
+          <input type="text" name="login" id="login" />
+          <label for="login">Username</label>
+        </div>
+      </div>
 
-    <input type="text" name="login" id="login" />
-    <label for="login">Username</label>
+      {#if recent_users}
+        <ul class="collection">
+          {#each recent_users as u }
+          <li class="collection-item avatar" class:active={selected.id === u.id} on:click={() => selected = u}>
+            <img src="/foo{u.picture}" alt="" class="circle">
+            <span class="title">{u.name}</span>
+            <p>{u.email}</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+          </li>
+          {/each}
+        </ul>
+      {:else}
+        <p class="loading">loading...</p>
+      {/if}
+
+    </div>
+
+    <div class="card-action row">
+      <button class="btn col s12 mex-primary-color">Login</button>
+    </div>
 
   </div>
-</div>
-
-{#if recent_users}
-  <ul class="collection">
-    {#each recent_users as u }
-    <li class="collection-item avatar" class:active={selected.id === u.id} on:click={() => selected = u}>
-      <img src="/foo{u.picture}" alt="" class="circle">
-      <span class="title">{u.name}</span>
-      <p>{u.email}</p>
-      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-    </li>
-    {/each}
-  </ul>
-{:else}
-  <p class="loading">loading...</p>
-{/if}
-
-  <div class="card-action row">
-    <button
-      class="btn col s12 mex-btn-primary"
-    >Login</button>
-  </div>
-
 </form>
-
-</div>
 
